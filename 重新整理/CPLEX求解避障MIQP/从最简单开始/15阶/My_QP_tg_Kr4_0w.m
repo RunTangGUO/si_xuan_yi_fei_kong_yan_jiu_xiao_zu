cad9a6486ad5d8022bb6443ce1ebd52c0d8e47f8
RunTangGUO <425367286@qq.com>
2018-07-10 09:29:16 +0800

    a
    a

---
+++ 重新整理/CPLEX求解避障MIQP/从最简单开始/15阶/My_QP_tg_Kr4_0w.m
@@ -0,0 +1,85 @@
+clear all;
+syms x;
+p=sym(zeros(16,1));
+for i=1:1:16
+    p(i,1)=x^(i-1);
+end
+syms x01;syms x11;syms x12;syms x13;syms x14;syms x15;syms x16;syms x17;syms x18;syms x19;syms x110;syms x111;syms x112;syms x113;syms x114;syms x115;
+syms y01;syms y11;syms y12;syms y13;syms y14;syms y15;syms y16;syms y17;syms y18;syms y19;syms y110;syms y111;syms y112;syms y113;syms y114;syms y115;
+rTp=[x01 x11 x12 x13 x14 x15 x16 x17 x18 x19 x110 x111 x112 x113 x114 x115;...
+     y01 y11 y12 y13 y14 y15 y16 y17 y18 y19 y110 y111 y112 y113 y114 y115];
+rT1=sym(zeros(2,1));
+for i=1:1:16
+    rT1(1,1)=rT1(1,1)+rTp(1,i)*p(i,1);
+    rT1(2,1)=rT1(2,1)+rTp(2,i)*p(i,1);
+end
+min=int(diff(rT1(1,1),2)^2+diff(rT1(2,1),2)^2,'x',0,1);
+rT10x=subs(rT1(1,1),{'x'},{0});
+rT10y=subs(rT1(2,1),{'x'},{0});
+rT11x=subs(rT1(1,1),{'x'},{1});
+rT11y=subs(rT1(2,1),{'x'},{1});
+%测试数值为0在端点
+dx11=limit(diff(rT1(1,1),1),x,0);
+dx21=limit(diff(rT1(1,1),1),x,1);
+dy11=limit(diff(rT1(2,1),1),x,0);
+dy21=limit(diff(rT1(2,1),1),x,1);
+dx12=limit(diff(rT1(1,1),2),x,0);
+dx22=limit(diff(rT1(1,1),2),x,1);
+dy12=limit(diff(rT1(2,1),2),x,0);
+dy22=limit(diff(rT1(2,1),2),x,1);
+dy13=limit(diff(rT1(2,1),3),x,0);
+dy23=limit(diff(rT1(2,1),3),x,1);
+dx13=limit(diff(rT1(1,1),3),x,0);
+dx23=limit(diff(rT1(1,1),3),x,1);
+dy14=limit(diff(rT1(2,1),4),x,0);
+dy24=limit(diff(rT1(2,1),4),x,1);
+dx14=limit(diff(rT1(1,1),4),x,0);
+dx24=limit(diff(rT1(1,1),4),x,1);
+%转折点处导数连续
+% dx1x2=limit(diff(subs(rT1(1,1),{'T0','T1'},{0,1}),1),x,1)-limit(diff(subs(rT2(1,1),{'T1','T2'},{1,2}),1),x,1);
+% dy1y2=limit(diff(subs(rT1(2,1),{'T0','T1'},{0,1}),1),x,1)-limit(diff(subs(rT2(2,1),{'T1','T2'},{1,2}),1),x,1);
+% dz1z2=limit(diff(subs(rT1(3,1),{'T0','T1'},{0,1}),1),x,1)-limit(diff(subs(rT2(3,1),{'T1','T2'},{1,2}),1),x,1);
+Q=sym(zeros(22));
+rTpL=[x01 x11 x12 x13 x14 x15 x16 x17 x18 x19 x110 x111 x112 x113 x114 x115,...
+     y01 y11 y12 y13 y14 y15 y16 y17 y18 y19 y110 y111 y112 y113 y114 y115];
+ for i=1:1:32
+     for j=1:1:i
+         if i==j
+             Q(i,i)=min;
+             for k=1:1:32
+                 if k~=i
+                     Q(i,i)=subs(Q(i,i),rTpL(k),0);
+                 end
+             end
+             Q(i,i)=2*Q(i,i)/rTpL(i)^2;
+         else
+             Q(i,j)=diff(diff(min,rTpL(1,i),1),rTpL(1,j),1);
+             Q(j,i)=Q(i,j);
+         end
+     end
+ end
+ H=sym(zeros(1,32));
+ for i=1:1:32
+     H(1,i)=min;
+     for j=1:1:32
+         if j~=i
+             H(1,i)=subs(H(1,i),rTpL(1,j),0);
+         end
+     end
+     H(1,i)=subs(H(1,i)-rTpL(1,i)^2*Q(i,i)/2,rTpL(1,i),1);
+ end
+STP=[rT10x rT10y rT11x rT11y dx11 dy11 dx21 dy21 dx12 dx22 dy12 dy22 dy13 dy23 dx13 dx23 dy14 dy24 dx14 dx24];
+STA=[1 1 5 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+coefficient=sym(zeros(18,28));
+for i=1:1:20
+    for j=1:1:32
+        coefficient(i,j)=STP(i);
+        for k=1:1:32
+            if k~=j
+                coefficient(i,j)=subs(coefficient(i,j),rTpL(k),0);
+            end
+        end
+        coefficient(i,j)=coefficient(i,j)/rTpL(j);
+    end
+end
+save QP_model
---
+++ 重新整理/CPLEX求解避障MIQP/从最简单开始/3阶_2/CPLEX_check_three_point.m
@@ -0,0 +1,133 @@
+clear;
+load QP_model;
+load ineq;
+x1 = sdpvar(12,1);
+%x2 = binvar(24,1);
+% x = [x1;x2];
+Q = double(Q);
+% Q(28:63,:)=0;
+% Q(:,28:63)=0;
+obj=x1'*Q*x1;
+AIneq=double(AIneq);
+% Aineq1=Aineq(:,1:27);
+%Aineq2=Aineq(:,28:51);
+% STb=[-5 6 -2 3];
+% bineq=STb';
+Aeq=double(coefficient);
+%STA=[1 3 1 5 3 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+%STA=[0 0 0 5 5 5 5 5 5 10 5 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+%STA=[1 1 5 5];
+STA=[1 1 5 1 0 0];
+beq=STA';
+Constraints=[];
+Constraints=[Constraints,Aeq*x1==beq];
+% Constraints=[Constraints,AIneq*x1==bineq];
+for i=1:4
+    Constraints=[Constraints,AIneq(i,:)*x1<=bineq(i)];%+Aineq2(i,:)*x2
+end
+%Constraints=[Aeq*x==beq,Aineq*x<=bineq];%,integer(x(28:63)),0<=x(28:63)<=1
+options=sdpsettings('solver','cplex');
+sol=optimize(Constraints,obj,options);
+X1= value(x1);
+double(x1)
+% X2=value(x2);
+% X=[X1' X2'];
+% X=X';
+for i=1:1:2
+     for j=1:1:6
+         rTp(i,j)=subs(rTp(i,j),rTp(i,j),X1((i-1)*6+j,1));
+     end
+ end
+ rTp=double(rTp);
+ rT1=sym([0;0;0]);
+for i=1:1:6
+    rT1(1,1)=subs(rT1(1,1)+rTp(1,i)*p(i,1));
+    rT1(2,1)=subs(rT1(2,1)+rTp(2,i)*p(i,1));
+end
+% hold on;
+% lx=0.45;ly=0.45;lz=0.25;
+% PeachPuff=[1 0.8549 0.72549];
+% BlanchedAlmond=[1 0.92157 0.80392];
+% Linen=[0.98039 0.94118 0.90196];
+% Maroon=[0.6902 0.18824 0.37647];
+% Red=[1 0 0];
+% DarkOrange=[1 0.54902 0];
+x0=zeros(100,1);
+y0=zeros(100,1);
+z0=zeros(100,1);
+for i=1:100
+    x0(i,1)=double(subs(rT1(1,1),'x',i/100));
+    y0(i,1)=double(subs(rT1(2,1),'x',i/100));
+end
+plot(x0,y0,'-.sr');
+hold on;
+% x1=zeros(100,1);
+% y1=zeros(100,1);
+% z1=zeros(100,1);
+% for i=1:100
+%     x1(i,1)=double(subs(rT2(1,1),'x',(i+100)/100));
+%     y1(i,1)=double(subs(rT2(2,1),'x',(i+100)/100));
+%     z1(i,1)=double(subs(rT2(3,1),'x',(i+100)/100));
+% end
+% plot3(x1,y1,z1,'-.sr');
+x=1;
+y=1;
+plot(x,y,'o');
+text(x,y,'T0');
+% O=[x-0.225 y-0.225 z-0.175];
+% a=[0 lx lx 0  0 0  0  0  0 0  0  lx lx lx lx lx lx lx 0];
+% b=[0 0  0  0  0 0  ly ly 0 ly ly ly ly 0  0  ly ly ly ly];
+% c=[0 0  lz lz 0 lz lz 0  0 0  lz lz 0  0  lz lz 0  0  0];
+% plot3(a+O(1),b+O(2),c+O(3),'k');
+% hold on;fill3([0+O(1) 0+O(1) 0+O(1) 0+O(1) ],[0+O(2) ly+O(2) ly+O(2) 0+O(2)],[0+O(3) 0+O(3) lz+O(3) lz+O(3)],Maroon);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[ly+O(2) ly+O(2) ly+O(2) ly+O(2)],[0+O(3) lz+O(3) lz+O(3) 0+O(3)],Maroon);
+% hold on;fill3([lx+O(1) lx+O(1) lx+O(1) lx+O(1) ],[ly+O(2) ly+O(2) 0+O(2) 0+O(2)],[0+O(3) lz+O(3) lz+O(3) 0+O(3)],Maroon);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[0+O(2) 0+O(2) 0+O(2) 0+O(2)],[0+O(3) lz+O(3) lz+O(3) 0+O(3)],Maroon);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[0+O(2) ly+O(2) ly+O(2) 0+O(2)],[lz+O(3) lz+O(3) lz+O(3) lz+O(3)],Maroon);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[0+O(2) ly+O(2) ly+O(2) 0+O(2)],[0+O(3) 0+O(3) 0+O(3) 0+O(3)],Maroon);
+x=5;
+y=1;
+plot(x,y,'o');
+text(x,y,'T1');
+% O=[x-0.225 y-0.225 z-0.175];
+% a=[0 lx lx 0  0 0  0  0  0 0  0  lx lx lx lx lx lx lx 0];
+% b=[0 0  0  0  0 0  ly ly 0 ly ly ly ly 0  0  ly ly ly ly];
+% c=[0 0  lz lz 0 lz lz 0  0 0  lz lz 0  0  lz lz 0  0  0];
+% plot3(a+O(1),b+O(2),c+O(3),'k');
+% hold on;fill3([0+O(1) 0+O(1) 0+O(1) 0+O(1) ],[0+O(2) ly+O(2) ly+O(2) 0+O(2)],[0+O(3) 0+O(3) lz+O(3) lz+O(3)],Red);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[ly+O(2) ly+O(2) ly+O(2) ly+O(2)],[0+O(3) lz+O(3) lz+O(3) 0+O(3)],Red);
+% hold on;fill3([lx+O(1) lx+O(1) lx+O(1) lx+O(1) ],[ly+O(2) ly+O(2) 0+O(2) 0+O(2)],[0+O(3) lz+O(3) lz+O(3) 0+O(3)],Red);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[0+O(2) 0+O(2) 0+O(2) 0+O(2)],[0+O(3) lz+O(3) lz+O(3) 0+O(3)],Red);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[0+O(2) ly+O(2) ly+O(2) 0+O(2)],[lz+O(3) lz+O(3) lz+O(3) lz+O(3)],Red);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[0+O(2) ly+O(2) ly+O(2) 0+O(2)],[0+O(3) 0+O(3) 0+O(3) 0+O(3)],Red);
+% x=10;
+% y=5;
+% z=5;
+% plot3(x,y,z,'o');
+% text(x,y,z,'T2');
+% O=[x-0.225 y-0.225 z-0.175];
+% a=[0 lx lx 0  0 0  0  0  0 0  0  lx lx lx lx lx lx lx 0];
+% b=[0 0  0  0  0 0  ly ly 0 ly ly ly ly 0  0  ly ly ly ly];
+% c=[0 0  lz lz 0 lz lz 0  0 0  lz lz 0  0  lz lz 0  0  0];
+% plot3(a+O(1),b+O(2),c+O(3),'k');
+% hold on;fill3([0+O(1) 0+O(1) 0+O(1) 0+O(1) ],[0+O(2) ly+O(2) ly+O(2) 0+O(2)],[0+O(3) 0+O(3) lz+O(3) lz+O(3)],DarkOrange);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[ly+O(2) ly+O(2) ly+O(2) ly+O(2)],[0+O(3) lz+O(3) lz+O(3) 0+O(3)],DarkOrange);
+% hold on;fill3([lx+O(1) lx+O(1) lx+O(1) lx+O(1) ],[ly+O(2) ly+O(2) 0+O(2) 0+O(2)],[0+O(3) lz+O(3) lz+O(3) 0+O(3)],DarkOrange);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[0+O(2) 0+O(2) 0+O(2) 0+O(2)],[0+O(3) lz+O(3) lz+O(3) 0+O(3)],DarkOrange);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[0+O(2) ly+O(2) ly+O(2) 0+O(2)],[lz+O(3) lz+O(3) lz+O(3) lz+O(3)],DarkOrange);
+% hold on;fill3([0+O(1) 0+O(1) lx+O(1) lx+O(1) ],[0+O(2) ly+O(2) ly+O(2) 0+O(2)],[0+O(3) 0+O(3) 0+O(3) 0+O(3)],DarkOrange);
+% O=[2 0 0];
+% lx=1;
+% ly=5;
+% lz=5;
+% a=[0 lx lx 0  0 0  0  0  0 0  0  lx lx lx lx lx lx lx 0];
+% b=[0 0  0  0  0 0  ly ly 0 ly ly ly ly 0  0  ly ly ly ly];
+% c=[0 0  lz lz 0 lz lz 0  0 0  lz lz 0  0  lz lz 0  0  0];
+% plot3(a+O(1),b+O(2),c+O(3),'k');
+xlabel('XÖá');
+ylabel('YÖá');
+zlabel('ZÖá');
+grid on;
+grid minor;
+% axis([-5 8 -5 6]);
+hold off;
